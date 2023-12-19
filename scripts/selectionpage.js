ae920c2c7a48e772c2dd3332b99aee50157f72c7
Jesse Bremer <jsr.bremer@gmail.com>
2023-12-19 11:58:32 -0500

    Added routing for flowform to selection page

--- scripts/selectionpage.js
+++ scripts/selectionpage.js
@@ -1,9 +1,9 @@
 function preferenceChoice(event) {
     if (event === 1) {
-    window.location.href = 'freeform.html';
+        window.location.href = 'freeform.html';
     } else if (event === 2) {
-        console.log("flowform");
+        window.location.href= 'flowform.html';
     } else if (event === 3) {
-        console.log('rigidform')
+        console.log('rigidform.html')
     }
 }
